9bfa2e91a09bf832952eace9df4fb14a9b620716
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-01 20:57:52 +0200

    Quito importación no usada

--- src/app/services/corporative-page/contact.service.ts
+++ src/app/services/corporative-page/contact.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { ContactForm } from 'src/app/models/corporative-page/contactForm';
 import { environment } from 'src/environments/environment';
 
