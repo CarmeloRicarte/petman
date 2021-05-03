04c7f0dea6ca9225a61da59647f9b6f6cba53716
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-03 22:21:22 +0200

    Borro servicio de Toast de PrimeNG e instalo Toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+import { LanguageService } from './services/language.service';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { LanguageService } from './services/language.service';
+
 import { HomeModule } from './corporative/pages/home/home.module';
+import { ToastrModule } from 'ngx-toastr';
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
@@ -23,6 +26,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ToastrModule.forRoot(), // ToastrModule added
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
