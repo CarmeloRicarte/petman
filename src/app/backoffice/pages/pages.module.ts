2bc29a75bf6147ffc33eb9621f61eb24f86869d2
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-05 19:50:28 +0200

    Creado módulo para componentes compartidos

--- src/app/backoffice/pages/pages.module.ts
+++ src/app/backoffice/pages/pages.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PagesRoutingModule } from './pages-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
 
 import { PagesComponent } from './pages.component';
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
@@ -13,6 +15,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     PagesRoutingModule,
+    SharedModule
   ],
   providers: [],
 })
