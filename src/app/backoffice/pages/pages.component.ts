ff83281f926360fce69d3d6906b3287ae23ae645
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-05 20:52:21 +0200

    Creo componente y módulo para las páginas

--- src/app/backoffice/pages/pages.component.ts
+++ src/app/backoffice/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-pages',
@@ -7,10 +8,91 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class PagesComponent implements OnInit {
+  sidebarStatic!: boolean;
 
-  constructor() { }
+  sidebarActive = false;
+
+  staticMenuMobileActive!: boolean;
+
+  menuClick!: boolean;
+
+  topbarItemClick!: boolean;
+
+  activeTopbarItem: any;
+
+  topbarMenuActive!: boolean;
+
+  searchClick = false;
+
+  search = false;
+
+  menuHoverActive = false;
+
+  constructor(public app: AppComponent) { }
 
   ngOnInit(): void {
+  }
+
+
+  onLayoutClick() {
+    if (!this.topbarItemClick) {
+      this.activeTopbarItem = null;
+      this.topbarMenuActive = false;
+    }
+
+    if (!this.menuClick) {
+      if (this.staticMenuMobileActive) {
+        this.staticMenuMobileActive = false;
+      }
+
+      this.menuHoverActive = false;
+      this.unblockBodyScroll();
+    }
+
+    if (!this.searchClick) {
+      this.search = false;
+    }
+
+    this.searchClick = false;
+    this.topbarItemClick = false;
+    this.menuClick = false;
+  }
+
+  onMenuButtonClick(event: any) {
+    this.menuClick = true;
+    this.topbarMenuActive = false;
+
+    if (this.isMobile()) {
+      this.staticMenuMobileActive = !this.staticMenuMobileActive;
+      if (this.staticMenuMobileActive) {
+        this.blockBodyScroll();
+      } else {
+        this.unblockBodyScroll();
+      }
+    }
+
+    event.preventDefault();
+  }
+
+  isMobile() {
+    return window.innerWidth <= 991;
+  }
+
+  blockBodyScroll(): void {
+    if (document.body.classList) {
+      document.body.classList.add('blocked-scroll');
+    } else {
+      document.body.className += ' blocked-scroll';
+    }
+  }
+
+  unblockBodyScroll(): void {
+    if (document.body.classList) {
+      document.body.classList.remove('blocked-scroll');
+    } else {
+      document.body.className = document.body.className.replace(new RegExp('(^|\\b)' +
+        'blocked-scroll'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
   }
 
 }
--- src/app/backoffice/pages/pages.module.ts
+++ src/app/backoffice/pages/pages.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { PagesRoutingModule } from './pages-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
-
-import { PagesComponent } from './pages.component';
+// paginas
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+// componentes
+import { PagesComponent } from './pages.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     PagesRoutingModule,
-    SharedModule
+    SharedModule,
+    CommonModule
   ],
   providers: [],
 })
