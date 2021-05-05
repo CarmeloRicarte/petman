1745cc4e0f24df81aa090636b76af4ae675ffcf6
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-05 20:53:16 +0200

    Creo módulo para componentes compartidos y creo topbar, menú y footer

--- src/app/backoffice/shared/shared.module.ts
+++ src/app/backoffice/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopbarComponent } from './topbar/topbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TopbarComponent,
+    MenuComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TopbarComponent,
+    MenuComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
