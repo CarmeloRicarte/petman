77ae5e07fd87237a7b03d8140d74ef1cd4e9d56c
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-16 18:19:30 +0200

    Añadida funcionalidad a los servicios de clientes y proveedores

--- src/app/services/backoffice/cliente/cliente.service.ts
+++ src/app/services/backoffice/cliente/cliente.service.ts
@@ -1,9 +1,101 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Cliente } from '../../../models/backoffice/cliente.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  token = localStorage.getItem('tokenUsuario');
 
-  constructor() { }
+  constructor(public http: HttpClient, private toastr: ToastrService) { }
+
+  get headers(): Object {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+
+  /**
+   * Funcion para crear un cliente
+   * @param cliente datos obligatorios del cliente
+   */
+  crearCliente(cliente: Cliente) {
+    const url = `${environment.urlServicios}/clientes`;
+    return this.http.post(url, cliente, this.headers)
+      .pipe(
+        map((resp: any) => {
+          this.toastr.success(`Cliente ${resp.cliente.nombre} creado!`);
+        }, (err: any) => {
+          this.toastr.error(`Error al crear el cliente: ${err.error.msg}`);
+        })
+      );
+  }
+
+  /**
+   * Función para actualizar datos de un cliente
+   * @param cliente objeto con datos del cliente
+   */
+  actualizarCliente(cliente: Cliente) {
+    const url = `${environment.urlServicios}/clientes/${cliente.uid}?token=${this.token}`;
+    return this.http.put(url, cliente, this.headers)
+      .pipe(
+        map((resp: any) => {
+          return resp;
+        }, (err: any) => {
+          return err;
+        })
+      );
+  }
+
+  /**
+    * Funcion para obtener los clientes
+    */
+  obtenerClientes() {
+    const url = `${environment.urlServicios}/clientes`;
+    return this.http.get(url, this.headers);
+  }
+
+  obtenerCliente(cliente: Cliente) {
+    const url = `${environment.urlServicios}/clientes/${cliente.uid}`;
+    return this.http.get(url, this.headers);
+  }
+
+  /**
+   * Funcion para eliminar un cliente
+   * @param cliente cliente a eliminar
+   */
+  eliminarCliente(cliente: Cliente) {
+    const url = `${environment.urlServicios}/clientes/${cliente.uid}`;
+    return this.http.delete(url, this.headers)
+      .pipe(
+        map((resp: any) => {
+          return resp;
+        }, (err: any) => {
+          return err;
+        })
+      );
+
+  }
+
+  /**
+   * Metodo para eliminar varios clientes
+   * @param clientes array de clientes a eliminar
+   */
+  eliminarClientes(clientes: any[]) {
+    const url = `${environment.urlServicios}/clientes/deleteSelected`;
+    return this.http.post(url, clientes, this.headers)
+      .pipe(
+        map((resp: any) => {
+          return resp;
+        }, (err: any) => {
+          return err;
+        })
+      );
+  }
 }
--- src/app/services/backoffice/proveedor/proveedor.service.ts
+++ src/app/services/backoffice/proveedor/proveedor.service.ts
@@ -1,9 +1,101 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Proveedor } from '../../../models/backoffice/proveedor.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProveedorService {
+  token = localStorage.getItem('tokenUsuario');
 
-  constructor() { }
+  constructor(public http: HttpClient, private toastr: ToastrService) { }
+
+  get headers(): Object {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+
+  /**
+   * Funcion para crear un proveedor
+   * @param proveedor datos obligatorios del proveedor
+   */
+  crearProveedor(proveedor: Proveedor) {
+    const url = `${environment.urlServicios}/proveedores`;
+    return this.http.post(url, proveedor, this.headers)
+      .pipe(
+        map((resp: any) => {
+          this.toastr.success(`Proveedor ${resp.proveedor.nombre} creado!`);
+        }, (err: any) => {
+          this.toastr.error(`Error al crear el proveedor: ${err.error.msg}`);
+        })
+      );
+  }
+
+  /**
+   * Función para actualizar datos de un proveedor
+   * @param proveedor objeto con datos del proveedor
+   */
+  actualizarProveedor(proveedor: Proveedor) {
+    const url = `${environment.urlServicios}/proveedores/${proveedor.uid}?token=${this.token}`;
+    return this.http.put(url, proveedor, this.headers)
+      .pipe(
+        map((resp: any) => {
+          return resp;
+        }, (err: any) => {
+          return err;
+        })
+      );
+  }
+
+  /**
+    * Funcion para obtener los proveedores
+    */
+  obtenerProveedores() {
+    const url = `${environment.urlServicios}/proveedores`;
+    return this.http.get(url, this.headers);
+  }
+
+  obtenerProveedor(proveedor: Proveedor) {
+    const url = `${environment.urlServicios}/proveedores/${proveedor.uid}`;
+    return this.http.get(url, this.headers);
+  }
+
+  /**
+   * Funcion para eliminar un proveedor
+   * @param proveedor proveedor a eliminar
+   */
+  eliminarProveedor(proveedor: Proveedor) {
+    const url = `${environment.urlServicios}/proveedores/${proveedor.uid}`;
+    return this.http.delete(url, this.headers)
+      .pipe(
+        map((resp: any) => {
+          return resp;
+        }, (err: any) => {
+          return err;
+        })
+      );
+
+  }
+
+  /**
+   * Metodo para eliminar varios proveedores
+   * @param proveedores array de proveedores a eliminar
+   */
+  eliminarProveedores(proveedores: any[]) {
+    const url = `${environment.urlServicios}/proveedores/deleteSelected`;
+    return this.http.post(url, proveedores, this.headers)
+      .pipe(
+        map((resp: any) => {
+          return resp;
+        }, (err: any) => {
+          return err;
+        })
+      );
+  }
 }
