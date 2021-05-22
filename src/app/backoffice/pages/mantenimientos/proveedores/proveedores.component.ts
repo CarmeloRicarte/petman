de914fb2d1300057ff8d482e348de7bbba076580
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-22 18:11:56 +0200

    Crud de proveedores finalizado

---
+++ src/app/backoffice/pages/mantenimientos/proveedores/proveedores.component.ts
@@ -0,0 +1,261 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Proveedor } from 'src/app/models/backoffice/proveedor.model';
+import { ProveedorService } from 'src/app/services/service.index';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-proveedores',
+  templateUrl: './proveedores.component.html',
+  styleUrls: ['./proveedores.component.scss'],
+  styles: [
+    `
+      @media screen and (max-width: 960px) {
+        :host
+          ::ng-deep
+          .p-datatable.p-datatable-proveedores.p-datatable-tbody
+          > tr
+          > td:last-child {
+          text-align: center;
+        }
+
+        :host
+          ::ng-deep
+          .p-datatable.p-datatable-proveedores
+          .p-datatable-tbody
+          > tr
+          > td:nth-child(6) {
+          display: flex;
+        }
+      }
+    `,
+  ],
+})
+export class ProveedoresComponent implements OnInit {
+  proveedores: Proveedor[] = [];
+  proveedor!: Proveedor;
+
+  proveedoresDialogCrear!: boolean;
+  crearProveedorForm!: FormGroup;
+
+  proveedoresDialogEditar!: boolean;
+  editarProveedorForm!: FormGroup;
+
+  proveedoresSeleccionados: any = [];
+
+  pulsadoSubmit!: boolean;
+  columnas: any[] = [];
+  totalProveedores = 0;
+
+  constructor(
+    private proveedorService: ProveedorService,
+    private fb: FormBuilder,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.cargarProveedores();
+    this.columnas = [
+      { field: 'nombre', header: 'Nombre' },
+      { field: 'categoria', header: 'Categoría' },
+      { field: 'poblacion', header: 'Población' },
+      { field: 'direccion', header: 'Dirección' },
+      { field: 'telefono', header: 'Teléfono' },
+      { field: 'cif', header: 'CIF' },
+      { field: 'email', header: 'Email' },
+      { field: 'web', header: 'Web' },
+    ];
+    this.crearProveedorForm = this.fb.group({
+      nombre: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      poblacion: new FormControl('', Validators.required),
+      direccion: new FormControl('', Validators.required),
+      telefono: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+        Validators.maxLength(9),
+        Validators.minLength(9),
+      ]),
+      cif: new FormControl('', [
+        Validators.maxLength(9),
+        Validators.minLength(9),
+        Validators.required,
+      ]),
+      email: new FormControl('', Validators.email),
+      web: new FormControl(''),
+    });
+
+    this.editarProveedorForm = this.fb.group({
+      nombre: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      poblacion: new FormControl('', Validators.required),
+      direccion: new FormControl('', Validators.required),
+      telefono: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+        Validators.maxLength(9),
+        Validators.minLength(9),
+      ]),
+      cif: new FormControl('', [
+        Validators.maxLength(9),
+        Validators.minLength(9),
+        Validators.required,
+      ]),
+      email: new FormControl('', Validators.email),
+      web: new FormControl(''),
+    });
+  }
+
+  get crearProveedorF() {
+    return this.crearProveedorForm.controls;
+  }
+
+  get editarProveedorF() {
+    return this.editarProveedorForm.controls;
+  }
+
+  /**
+   * metodo para obtener los proveedores
+   */
+  cargarProveedores() {
+    this.proveedorService.obtenerProveedores().subscribe(
+      (res: any) => {
+        this.proveedores = res.proveedores;
+        this.totalProveedores = res.total;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  crearProveedor(): void {
+    this.pulsadoSubmit = false;
+    this.crearProveedorForm.reset();
+    this.proveedoresDialogCrear = true; // abrimos el cuadro de dialogo para crear el proveedor
+  }
+
+  editarProveedor(proveedor: Proveedor): void {
+    this.proveedor = { ...proveedor };
+    this.editarProveedorForm.reset();
+    this.proveedoresDialogEditar = true;
+  }
+
+  guardarProveedor(proveedor: any, accion: 'crear' | 'editar'): void {
+    this.pulsadoSubmit = true;
+    if (accion === 'editar') {
+      if (this.editarProveedorForm.invalid) {
+        // si el formulario no es correcto, para ejecucion
+        return;
+      }
+      this.proveedorService.actualizarProveedor(proveedor).subscribe(
+        (res: any) => {
+          this.toastr.success(`Proveedor ${res.proveedor.nombre} actualizado!`);
+          this.ocultarProveedoresDialog(accion);
+          this.cargarProveedores();
+        },
+        (err: any) => {
+          console.log(err);
+          this.ocultarProveedoresDialog(accion);
+          this.cargarProveedores();
+          this.toastr.error(
+            `Error al actualizar el proveedor: ${err.error.msg}`
+          );
+        }
+      );
+    } else {
+      if (this.crearProveedorForm.invalid) {
+        // si el formulario no es correcto, para ejecucion
+        return;
+      }
+      this.proveedorService.crearProveedor(proveedor).subscribe(
+        (res: any) => {
+          this.toastr.success(`Proveedor ${res.proveedor.nombre} creado!`);
+          this.ocultarProveedoresDialog(accion);
+          this.cargarProveedores();
+        },
+        (err: any) => {
+          this.toastr.error(`Error al crear el proveedor: ${err.error.msg}`);
+          this.ocultarProveedoresDialog(accion);
+          this.cargarProveedores();
+        }
+      );
+    }
+  }
+
+  /**
+   * Metodo que llama al servicio para eliminar un proveedor
+   * @param proveedor proveedor a eliminar
+   */
+  eliminarProveedor(proveedor: Proveedor) {
+    Swal.fire({
+      title: '¿Borrar proveedor?',
+      text: `Está a punto de borrar a ${proveedor.nombre}`,
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrarlo!',
+    }).then((result: any) => {
+      if (result.value) {
+        this.proveedorService.eliminarProveedor(proveedor).subscribe(
+          () => {
+            Swal.fire(
+              'Eliminado!',
+              `${proveedor.nombre} fue eliminado correctamente`,
+              'success'
+            );
+            this.cargarProveedores();
+          },
+          (err: any) => {
+            console.error(err);
+          }
+        );
+      }
+    });
+  }
+
+  eliminarProveedoresSeleccionados() {
+    Swal.fire({
+      title: '¿Borrar proveedores seleccionados?',
+      text: `Está a punto de borrar a ${this.proveedoresSeleccionados.length} proveedores, está seguro?`,
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrarlos!',
+    }).then((result: any) => {
+      if (result.value) {
+        this.proveedorService
+          .eliminarProveedores(this.proveedoresSeleccionados)
+          .subscribe(
+            (res) => {
+              Swal.fire('Eliminados!', `${res.msg}`, 'success');
+              this.cargarProveedores();
+            },
+            (err: any) => {
+              console.error(err);
+            }
+          );
+      }
+    });
+  }
+
+  ocultarProveedoresDialog(accion: 'crear' | 'editar') {
+    if (accion === 'crear') {
+      this.proveedoresDialogCrear = false;
+    } else {
+      this.proveedoresDialogEditar = false;
+    }
+
+    this.pulsadoSubmit = false;
+  }
+}
--- src/app/services/backoffice/proveedor/proveedor.service.ts
+++ src/app/services/backoffice/proveedor/proveedor.service.ts
@@ -1,24 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Proveedor } from '../../../models/backoffice/proveedor.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProveedorService {
   token = localStorage.getItem('tokenUsuario');
 
-  constructor(public http: HttpClient, private toastr: ToastrService) { }
+  constructor(public http: HttpClient, private toastr: ToastrService) {}
 
   get headers(): Object {
     return {
       headers: {
-        'x-token': this.token
-      }
-    }
+        'x-token': this.token,
+      },
+    };
   }
 
   /**
@@ -27,14 +28,12 @@ export class ProveedorService {
    */
   crearProveedor(proveedor: Proveedor) {
     const url = `${environment.urlServicios}/proveedores`;
-    return this.http.post(url, proveedor, this.headers)
-      .pipe(
-        map((resp: any) => {
-          this.toastr.success(`Proveedor ${resp.proveedor.nombre} creado!`);
-        }, (err: any) => {
-          this.toastr.error(`Error al crear el proveedor: ${err.error.msg}`);
-        })
-      );
+    return this.http.post(url, proveedor, this.headers).pipe(
+      map(
+        (resp: any) => resp,
+        (err: any) => err
+      )
+    );
   }
 
   /**
@@ -43,19 +42,21 @@ export class ProveedorService {
    */
   actualizarProveedor(proveedor: Proveedor) {
     const url = `${environment.urlServicios}/proveedores/${proveedor.uid}?token=${this.token}`;
-    return this.http.put(url, proveedor, this.headers)
-      .pipe(
-        map((resp: any) => {
+    return this.http.put(url, proveedor, this.headers).pipe(
+      map(
+        (resp: any) => {
           return resp;
-        }, (err: any) => {
+        },
+        (err: any) => {
           return err;
-        })
-      );
+        }
+      )
+    );
   }
 
   /**
-    * Funcion para obtener los proveedores
-    */
+   * Funcion para obtener los proveedores
+   */
   obtenerProveedores() {
     const url = `${environment.urlServicios}/proveedores`;
     return this.http.get(url, this.headers);
@@ -72,15 +73,16 @@ export class ProveedorService {
    */
   eliminarProveedor(proveedor: Proveedor) {
     const url = `${environment.urlServicios}/proveedores/${proveedor.uid}`;
-    return this.http.delete(url, this.headers)
-      .pipe(
-        map((resp: any) => {
+    return this.http.delete(url, this.headers).pipe(
+      map(
+        (resp: any) => {
           return resp;
-        }, (err: any) => {
+        },
+        (err: any) => {
           return err;
-        })
-      );
-
+        }
+      )
+    );
   }
 
   /**
@@ -89,13 +91,15 @@ export class ProveedorService {
    */
   eliminarProveedores(proveedores: any[]) {
     const url = `${environment.urlServicios}/proveedores/deleteSelected`;
-    return this.http.post(url, proveedores, this.headers)
-      .pipe(
-        map((resp: any) => {
+    return this.http.post(url, proveedores, this.headers).pipe(
+      map(
+        (resp: any) => {
           return resp;
-        }, (err: any) => {
+        },
+        (err: any) => {
           return err;
-        })
-      );
+        }
+      )
+    );
   }
 }
