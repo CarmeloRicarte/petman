d078ccacd86fef52ce71f7bcd53131874b16137e
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2021-05-23 10:42:23 +0200

    añadidos servicios al index de servicios

--- src/app/services/service.index.ts
+++ src/app/services/service.index.ts
@@ -4,3 +4,6 @@ export { UsuarioService } from './backoffice/usuario/usuario.service';
 export { MenuService } from './backoffice/menu/menu.service';
 export { ClienteService } from './backoffice/cliente/cliente.service';
 export { ProveedorService } from './backoffice/proveedor/proveedor.service';
+export { CategoriaService } from './backoffice/categoria/categoria.service';
+export { SubsubcategoriaService } from './backoffice/subcategoria/subcategoria.service';
+export { ProductoService } from './backoffice/producto/producto.service';
